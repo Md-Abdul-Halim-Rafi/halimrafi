e53872a215ed84f5cc0f5ec244c5ba2cc84f47d4
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-04-14 10:36:24 +0600

    about started

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,16 +3,10 @@ import AnimatedText from "@/component/AnimatedText";
 export default function Home() {
 	return (
 		<main className="px-32 py-8 w-full h-full bg-light">
-			<p className="text-4xl font-semibold">
-				Hi, I am
-			</p>
 			<AnimatedText
-				text="Md Abdul Halim Rafi"
+				text="Passion Fuels Purpose!"
 				className="!text-8xl !text-left xl:!text-8xl font-black lg:!text-6xl md:!text-5xl sm:!text-3xl"
 			/>
-			<h2 className="text-2xl font-semibold">
-				Driven individual with a passion for technology and entrepreneurship
-			</h2>
 		</main>
 	)
 }
